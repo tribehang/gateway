276fed899e8db799ffd06d0df9d39c66b46f6100
Saeed <hatrena@gmail.com>
2018-01-14 20:33:05 +0100

    add user photo

--- app/Http/Transformers/ProfileImageTransformer.php
+++ app/Http/Transformers/ProfileImageTransformer.php
@@ -7,8 +7,12 @@ use League\Fractal\TransformerAbstract;
 
 class ProfileImageTransformer extends TransformerAbstract
 {
-    public function transform(ProfileImage $profileImage): array
+    public function transform(ProfileImage $profileImage = null): array
     {
+        if (is_null($profileImage)) {
+            return [];
+        }
+
         return [
             'id' => $profileImage->id,
         ];
--- app/Http/Transformers/UserTransformer.php
+++ app/Http/Transformers/UserTransformer.php
@@ -3,10 +3,15 @@
 namespace App\Http\Transformers;
 
 use App\Models\User;
+use League\Fractal\Resource\Item;
 use League\Fractal\TransformerAbstract;
 
 class UserTransformer extends TransformerAbstract
 {
+    protected $defaultIncludes = [
+        'profileImage',
+    ];
+
     public function transform(User $user): array
     {
         return [
@@ -14,5 +19,12 @@ class UserTransformer extends TransformerAbstract
             'name' => $user->name,
             'email' => $user->email,
         ];
+    }
+
+    public function includeProfileImage(User $user): Item
+    {
+        return $this->item(
+            $user->profileImage, new ProfileImageTransformer()
+        );
     }
 }
